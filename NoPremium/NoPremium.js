a7dfcfc6e65553a32d119c340a9f806f31a48c18
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-07-07 12:55:00 +0200

    removes audio ads

--- NoPremium/NoPremium.js
+++ NoPremium/NoPremium.js
@@ -1,10 +1,18 @@
+const { join } = require('path');
+
 module.exports = {
   name: 'No Premium',
   description: 'Removes all ads (banners, audio) and offers to subscribe to Deezer Premium',
-  start({ asar }) {
+  start({ asar, paths }) {
     // Hide ads
     asar.injectCss(`
     .conversion-banner, .ads { display: none !important; }
     `);
+    // Removes audio ads
+    replaceInFile(
+      join(paths.extractedAsar, 'build', 'assets', 'cache', 'js', 'route-naboo.fda0f9eaad2eeb36f5b5.js'),
+      /this.props.hasAudioAds|e.user.USER.OPTIONS.ads_audio/g,
+      'false'
+    );
   }
 }
