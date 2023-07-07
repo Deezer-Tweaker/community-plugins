e524f77aefdd392be7d9b9d44982de177dde97ea
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-07-07 14:07:36 +0200

    no premium plugin

--- NoPremium/NoPremium.js
+++ NoPremium/NoPremium.js
@@ -3,13 +3,14 @@ const { join } = require('path');
 module.exports = {
   name: 'No Premium',
   description: 'Removes all ads (banners, audio) and offers to subscribe to Deezer Premium',
-  start({ asar, paths }) {
+  start({ asar, paths, startingFrom }) {
+    if (startingFrom === 'app_start') return;
     // Hide ads
     asar.injectCss(`
     .conversion-banner, .ads { display: none !important; }
     `);
     // Removes audio ads
-    replaceInFile(
+    asar.replaceInFile(
       join(paths.extractedAsar, 'build', 'assets', 'cache', 'js', 'route-naboo.fda0f9eaad2eeb36f5b5.js'),
       /this.props.hasAudioAds|e.user.USER.OPTIONS.ads_audio/g,
       'false'
