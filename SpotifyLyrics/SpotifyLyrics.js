38288fd240c64907013e6fd14cb88168f06e8a13
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-07-09 01:13:58 +0200

    show button if lyrics were found

--- SpotifyLyrics/SpotifyLyrics.js
+++ SpotifyLyrics/SpotifyLyrics.js
@@ -37,9 +37,9 @@ module.exports = {
     },
     {
       file: '%jsCache%/route-naboo.fda0f9eaad2eeb36f5b5.js',
-      find: /(Object\(ve.g\)\(\);)/g,
-      replace: '$1console.log(n,a,s,c,l,d,p,m,b,g,y,v,_);'
-    }
+      find: /(null===t\|\|"0"===t)/g,
+      replace: '$1 || !lyricsFound'
+    },
   ],
   inject({ Api, React }) {}
 }
