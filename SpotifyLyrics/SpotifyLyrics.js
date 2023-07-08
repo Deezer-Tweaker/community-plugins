7789c6c9fdccfc8b36dac314ac886301e21aca8b
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-07-08 21:14:16 +0200

    fetch lyrics + access token

--- SpotifyLyrics/SpotifyLyrics.js
+++ SpotifyLyrics/SpotifyLyrics.js
@@ -6,19 +6,32 @@ module.exports = {
       file: '%jsCache%/route-naboo.fda0f9eaad2eeb36f5b5.js',
       find: /(Ue.ShowLyrics=\(\{item:e,lyricsId:t,onPlayLyrics:r}\)=>\{)/g,
       replace: `$1
+      const accessToken = DeezerTweaker.Plugins['SpotifyLyrics'].Settings.get('access_token');
+      async function fetchLyrics(accessToken) {
+        const results = await fetch(\`https://api.spotify.com/v1/search?q=track%3A\${e.title}+artist%3A\${e.artist.name}&type=track&limit=1\`, {
+          headers: { Authorization: 'Bearer ' + accessToken }
+        });
+        console.log(results);
+      }
       // No lyrics are available on Deezer 
       if (t === null) {
-        fetch('https://accounts.spotify.com/api/token', { 
-          headers: { Authorization: 'Basic ' + btoa('782b7e0e60444f8d935e0ef5250819ad:4d98697f674f4f5b823604c7f36d6eee'), 'Content-Type': 'application/x-www-form-urlencoded' },
-          body: 'grant_type=client_credentials',
-          method: 'POST'
-        }).then(res => res.json()).then(json => {
-          DeezerTweaker.Plugins['SpotifyLyrics'].Settings.set('access_token', json.access_token);
-          DeezerTweaker.Plugins['SpotifyLyrics'].Settings.set('expires_at', Date.now() + json.expires_in);
-          /*fetch(\`https://api.spotify.com/v1/search?q=track%3A\${e.title}+artist%3A\${e.artist.name}&type=track&limit=1\`, {
-            headers: { Authorization: 'Bearer ' + json.access_token }
-          }).then(console.log);*/
-        });
+        if (!accessToken || DeezerTweaker.Plugins['SpotifyLyrics'].Settings.get('expires_at') < Date.now()) {
+          fetch('https://accounts.spotify.com/api/token', { 
+            headers: { 
+              Authorization: 'Basic ' + btoa('782b7e0e60444f8d935e0ef5250819ad:4d98697f674f4f5b823604c7f36d6eee'), 
+              'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'grant_type=client_credentials',
+            method: 'POST'
+          }).then(res => res.json()).then(json => {
+            DeezerTweaker.Plugins['SpotifyLyrics'].Settings.set('access_token', json.access_token);
+            DeezerTweaker.Plugins['SpotifyLyrics'].Settings.set('expires_at', Date.now() + json.expires_in);
+            console.log('Saved access token');
+            fetchLyrics(json.access_token);
+          });
+        } else {
+          fetchLyrics(accessToken);
+        }
       }
       `
     },
